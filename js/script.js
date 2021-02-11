0080d0c6b97116d7b5cbe81122a67ea0035a7917
Diego <diego.scano77@gmail.com>
2021-02-11 12:30:52 +0100

    problema milestone2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#root',
   data: {
     userSearch: '',
-    movies: []
+    all: [],
+    movies: [],
+    tvShows: []
   },
   methods: {
     searchMovies: function() {
@@ -11,7 +13,26 @@ new Vue({
         .get('https://api.themoviedb.org/3/search/movie?api_key=109b8dfeb232ae967f413b6c5604382e&query=' + self.userSearch)
         .then(function(resp) {
           self.movies = resp.data.results;
-        })
+          self.all = [...self.movies, ...self.all];
+        });
+    },
+    searchTvShows: function() {
+      const self = this;
+      axios
+        .get('https://api.themoviedb.org/3/search/tv?api_key=109b8dfeb232ae967f413b6c5604382e&query=' + self.userSearch)
+        .then(function(resp) {
+          self.tvShows = resp.data.results;
+          self.all = [...self.tvShows, ...self.all];
+        });
+    },
+    search: function() {
+      this.all = [];
+      this.searchMovies();
+      this.searchTvShows();
+      console.log(this.all);
+    },
+    getVote: function(vote) {
+      return parseInt(vote / 2);
     }
   }
 })
